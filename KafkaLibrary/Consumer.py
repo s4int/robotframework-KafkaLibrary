3b6e0cabc979efaedcbf41b1926a12bc8ff006fa
Jarimatti Valkonen <jarimatti.valkonen@spartaconsulting.fi>
2018-10-30 14:15:19 +0200

    Allow multiple named Kafka consumers to exist.
    The consumers are referenced by name argument that is added to each consumer keyword.
    The default client name is 'default' if no name argument is provided.
    
    A consumer dict is kept in the Consumer object. The dictionary stores customer instances
    by name. An instance is removed from the dictionary when `Close` keyword is called.

--- KafkaLibrary/Consumer.py
+++ KafkaLibrary/Consumer.py
@@ -2,10 +2,11 @@ from kafka import KafkaConsumer, TopicPartition
 
 
 class Consumer(object):
-    consumer = None
+    consumers = {}
 
     def connect_consumer(
             self,
+            name='default',
             bootstrap_servers='127.0.0.1:9092',
             client_id='Robot',
             group_id=None,
@@ -16,6 +17,10 @@ class Consumer(object):
         """Connect kafka consumer.
     
         Keyword Arguments:
+        - ``name`` (str): name of this consumer instance. Use the name to
+            identify the client instance in keyword calls. If an existing
+            client with this name exists, it will be replaced.
+            Default: 'default'.
         - ``bootstrap_servers``: 'host[:port]' string (or list of 'host[:port]'
             strings) that the consumer should contact to bootstrap initial
             cluster metadata. This does not have to be the full node list.
@@ -43,7 +48,7 @@ class Consumer(object):
         http://kafka-python.readthedocs.io/en/master/apidoc/KafkaConsumer.html
         """
 
-        self.consumer = KafkaConsumer(
+        consumer = KafkaConsumer(
             bootstrap_servers=bootstrap_servers,
             auto_offset_reset=auto_offset_reset,
             client_id=client_id,
@@ -51,106 +56,120 @@ class Consumer(object):
             enable_auto_commit=enable_auto_commit,
             **kwargs
         )
+        self.consumers[name] = consumer
 
-    def _is_assigned(self, topic_partition):
+    def _is_assigned(self, consumer, topic_partition):
         for tp in topic_partition:
-            if tp in self.consumer.assignment():
+            if tp in consumer.assignment():
                 return True
         return False
 
-    def get_kafka_topics(self):
+    def get_kafka_topics(self, name='default'):
         """Return list of kafka topics.
+
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
-        return list(self.consumer.topics())
+        return list(self.consumers[name].topics())
 
-    def get_kafka_partitions_for_topic(self, topic=None):
+    def get_kafka_partitions_for_topic(self, topic=None, name='default'):
         """Retrun list of partitions for kafka topic.
         
         - ``topic`` (str): Topic to check.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
-        return list(self.consumer.partitions_for_topic(topic))
+        return list(self.consumers[name].partitions_for_topic(topic))
 
-    def assign_to_topic_partition(self, topic_partition=None):
+    def assign_to_topic_partition(self, topic_partition=None, name='default'):
         """Assign a list of TopicPartitions to this consumer.
         
         - ``partitions`` (list of `TopicPartition`): Assignment for this instance.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
             topic_partition = [topic_partition]
-        if not self._is_assigned(topic_partition):
-            self.consumer.assign(topic_partition)
+        consumer = consumers[name]
+        if not self._is_assigned(consumer, topic_partition):
+            consumer.assign(topic_partition)
 
-    def subscribe_topic(self, topics=[], pattern=None):
+    def subscribe_topic(self, topics=[], pattern=None, name='default'):
         """Subscribe to a list of topics, or a topic regex pattern.
         
         - ``topics`` (list): List of topics for subscription.
-        - ``pattern`` (str): Pattern to match available topics. You must provide either topics or pattern,
-          but not both.
+        - ``pattern`` (str): Pattern to match available topics. You must
+          provide either topics or pattern, but not both.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if not isinstance(topics, list):
             topics = [topics]
-        self.consumer.subscribe(topics, pattern=pattern)
+        self.consumers[name].subscribe(topics, pattern=pattern)
 
-    def get_position(self, topic_partition=None):
+    def get_position(self, topic_partition=None, name='default'):
         """Return offset of the next record that will be fetched.
         
         - ``topic_partition`` (TopicPartition): Partition to check
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
-            return self.consumer.position(topic_partition)
+            return self.consumers[name].position(topic_partition)
         else:
             raise TypeError("topic_partition must be of type TopicPartition, create it with Create TopicPartition keyword.")
 
-    def seek(self, offset, topic_partition=None):
+    def seek(self, offset, topic_partition=None, name='default'):
         """Manually specify the fetch offset for a TopicPartition.
         
         - ``offset``: Message offset in partition
         - ``topic_partition`` (`TopicPartition`): Partition for seek operation
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
-            self.consumer.seek(topic_partition, offset=offset)
+            self.consumers[name].seek(topic_partition, offset=offset)
         else:
             raise TypeError("topic_partition must be of type TopicPartition, create it with Create TopicPartition keyword.")
 
-    def seek_to_beginning(self, topic_partition=None):
+    def seek_to_beginning(self, topic_partition=None, name='default'):
         """Seek to the oldest available offset for partitions.
         
         - ``topic_partition``: Optionally provide specific TopicPartitions,
           otherwise default to all assigned partitions.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
-            self.consumer.seek_to_beginning(topic_partition)
+            self.consumers[name].seek_to_beginning(topic_partition)
         else:
             raise TypeError("topic_partition must be of type TopicPartition, create it with Create TopicPartition keyword.")
 
-    def seek_to_end(self, topic_partition=None):
+    def seek_to_end(self, topic_partition=None, name='default'):
         """Seek to the most recent available offset for partitions.
         
         - ``topic_partition``: Optionally provide specific `TopicPartitions`,
           otherwise default to all assigned partitions.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
-            self.consumer.seek_to_end(topic_partition)
+            self.consumers[name].seek_to_end(topic_partition)
         else:
             raise TypeError("topic_partition must be of type TopicPartition, create it with Create TopicPartition keyword.")
 
-    def get_assigned_partitions(self):
+    def get_assigned_partitions(self, name='default'):
         """Return `TopicPartitions` currently assigned to this consumer.
-        """
-        return list(self.consumer.assignment())
 
-    def get_number_of_messages_in_topics(self, topics):
+        - ``name`` (str): consumer instance name. Default: 'default'.
+        """
+        return list(self.consumers[name].assignment())
+
+    def get_number_of_messages_in_topics(self, topics, name='default'):
         """Retrun number of messages in topics.
         
         - ``topics`` (list): list of topics.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if not isinstance(topics, list):
@@ -158,51 +177,54 @@ class Consumer(object):
 
         number_of_messages = 0
         for t in topics:
-            part = self.get_kafka_partitions_for_topic(topic=t)
+            part = self.get_kafka_partitions_for_topic(topic=t, name=name)
             Partitions = map(lambda p: TopicPartition(topic=t, partition=p), part)
-            number_of_messages += self.get_number_of_messages_in_topicpartition(Partitions)
+            number_of_messages += self.get_number_of_messages_in_topicpartition(Partitions, name=name)
 
         return number_of_messages
 
-    def get_number_of_messages_in_topicpartition(self, topic_partition=None):
+    def get_number_of_messages_in_topicpartition(self, topic_partition=None, name='default'):
         """Return number of messages in TopicPartition.
         
         - ``topic_partition`` (list of TopicPartition)
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
         if isinstance(topic_partition, TopicPartition):
             topic_partition = [topic_partition]
 
         number_of_messages = 0
-        assignment = self.consumer.assignment()
+        consumer = self.consumers[name]
+        assignment = consumer.assignment()
 
-        self.consumer.unsubscribe()
+        consumer.unsubscribe()
         for Partition in topic_partition:
             if not isinstance(Partition, TopicPartition):
                 raise TypeError("topic_partition must be of type TopicPartition, create it with Create TopicPartition keyword.")
 
-            self.assign_to_topic_partition(Partition)
+            self.assign_to_topic_partition(Partition, name=name)
 
-            self.consumer.seek_to_end(Partition)
-            end = self.consumer.position(Partition)
-            self.consumer.seek_to_beginning(Partition)
-            start = self.consumer.position(Partition)
+            consumer.seek_to_end(Partition)
+            end = consumer.position(Partition)
+            consumer.seek_to_beginning(Partition)
+            start = consumer.position(Partition)
             number_of_messages += end-start
 
-        self.consumer.unsubscribe()
-        self.consumer.assign(assignment)
+        consumer.unsubscribe()
+        consumer.assign(assignment)
         return number_of_messages
 
-    def poll(self, timeout_ms=0, max_records=None):
+    def poll(self, timeout_ms=0, max_records=None, name='default'):
         """Fetch data from assigned topics / partitions.
         
         - ``max_records`` (int): maximum number of records to poll. Default: Inherit value from max_poll_records.
         - ``timeout_ms`` (int): Milliseconds spent waiting in poll if data is not available in the buffer.
           If 0, returns immediately with any records that are available currently in the buffer, else returns empty.
           Must not be negative. Default: `0`
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
-        messages = self.consumer.poll(timeout_ms=timeout_ms, max_records=max_records)
+        messages = self.consumers[name].poll(timeout_ms=timeout_ms, max_records=max_records)
 
         result = []
         for _, msg in messages.items():
@@ -210,25 +232,33 @@ class Consumer(object):
                 result.append(item)
         return result
 
-    def commit(self, offsets=None):
+    def commit(self, offsets=None, name='default'):
         """Commit offsets to kafka, blocking until success or error.
 
         - ``offset`` (dict): `{TopicPartition: OffsetAndMetadata}` dict to commit with
           the configured group_id. Defaults to currently consumed offsets for all subscribed partitions.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
 
-        self.consumer.commit(offsets)
+        self.consumers[name].commit(offsets)
 
-    def committed(self, topic_partition):
+    def committed(self, topic_partition, name='default'):
         """Returns the last committed offset for the given partition, or None if there was no prior commit.
 
         - ``topic_partition`` (`TopicPartition`): The partition to check.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
-        return self.consumer.committed(topic_partition)
+        return self.consumers[name].committed(topic_partition)
 
-    def close(self, autocommit=True):
+    def close(self, autocommit=True, name='default'):
         """Close the consumer, waiting indefinitely for any needed cleanup.
 
+        The named consumer instance is no longer available for operations and
+        any attempts to use it will cause an exception.
+
         - ``autocommit`` (bool): Default `True`.
+        - ``name`` (str): consumer instance name. Default: 'default'.
         """
-        self.consumer.close(autocommit=autocommit)
+        val = self.consumers[name].close(autocommit=autocommit)
+        del self.consumers[name]
+        val
